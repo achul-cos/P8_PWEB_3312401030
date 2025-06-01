cb9b20c2aeee07b0be0e741b8dfdee9e49b8ef7e
achul-cos <waduhgaming554@gmail.com>
2025-06-01 21:57:54 +0700

    Praktikum 8

---
+++ resources/views/list_produk.blade.php
@@ -0,0 +1,41 @@
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    No
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Nama Produk
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Deskripsi Produk
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Harga Produk
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($nama as $index => $item)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ $index + 1 }}
+                </th>
+                <td class="px-6 py-4">
+                    {{ $item }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $desc[$index] }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $harga[$index] }}
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
